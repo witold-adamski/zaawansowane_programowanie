0c248feb2b9884db1da4d92222ce8f7f8638c214
witold-adamski <witold.adamski@edu.uekat.pl>
2021-11-28 08:43:58 +0100

    pycodestyle

--- Zad2.py
+++ Zad2.py
@@ -1,12 +1,14 @@
 def liczby(numbers):
     lista = []
     for number in numbers:
-        lista.append(number*2)
+        lista.append(number * 2)
     return lista
 
+
 def liczby2(numbers):
-    lista = [number*2 for number in numbers]
+    lista = [number * 2 for number in numbers]
     return lista
+
 
 lista2 = range(5)
 print(liczby(lista2))
