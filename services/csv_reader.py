fd9309c4c9a5a4b88f81a56dd02a0b767eb0d3c9
witold-adamski <93861602+witold-adamski@users.noreply.github.com>
2021-12-17 20:30:19 +0100

    Wyczyszczenie printów

--- services/csv_reader.py
+++ services/csv_reader.py
@@ -6,10 +6,8 @@ def read_csv(path, class_name):
     with open(path, 'r', errors="ignore") as read_obj:
         csv_reader = reader(read_obj)
         header = next(csv_reader)
-        print(header)
         if header is not None:
             for row in csv_reader:
-                print(*row)
                 obj = class_name(*row)
                 objects_list.append(obj.api())
     return objects_list
