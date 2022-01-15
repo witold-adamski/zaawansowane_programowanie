6ffebc7ff022f9bc9cf4758eb3888201c6cc293d
Ketiw <witold.adamski93@gmail.com>
2022-01-15 12:58:14 +0100

    poprawka do main

--- main.py
+++ main.py
@@ -7,6 +7,6 @@ developer_1 = Developer(1, 'Słoneczny developer', 'ul. Słoneczna 15',
                         '56845345')
 mieszkanie_1 = Mieszkanie(1, 50, 'ul. Radosna 1', 4000.93)
 dom_1 = Dom(1, 105, 'ul. Radosna 5', 9148.21)
-zamowienie_1 = Zamowienie(1, developer_1, [mieszkanie_1, dom_1], 'Wesołe '
-                                                               'osiedle')
+zamowienie_1 = Zamowienie(1, developer_1, [mieszkanie_1, dom_1],
+                          'Wesołe osiedle')
 print(zamowienie_1)
